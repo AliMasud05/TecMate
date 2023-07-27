39c50370ac7043959990af6827e5ed75559db60e
iamshakil19 <shakilahmed.pure@gmail.com>
2023-07-28 04:56:14 +0600

    navbar done

--- src/components/Layout/RootLayout.js
+++ src/components/Layout/RootLayout.js
@@ -1,33 +1,72 @@
-import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { Breadcrumb, Button, Layout, Menu, theme } from "antd";
 const { Header, Content, Footer } = Layout;
-
+import { DownOutlined, SmileOutlined } from "@ant-design/icons";
+import { Dropdown, Space } from "antd";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import logo from "../../assets/logo.png"
+import Image from "next/image";
 const RootLayout = ({ children }) => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+  const items = [
+    {
+      key: "1",
+      label: <Link href="/cpu">Processor</Link>,
+    },
+    {
+      key: "2",
+      label: <Link href="/motherboard">Motherboard</Link>,
+    },
+    {
+      key: "3",
+      label: <Link href="/ram">Ram</Link>,
+    },
+    {
+      key: "4",
+      label: <Link href="/psu">Power Supply</Link>,
+    },
+    {
+      key: "5",
+      label: <Link href="/storage">Storage Device</Link>,
+    },
+    {
+      key: "6",
+      label: <Link href="/monitor">Monitor</Link>,
+    },
+    {
+      key: "7",
+      label: <Link href="/others">Others</Link>,
+    },
+  ];
+  const router = useRouter();
+
   return (
-    <Layout className="layout">
-      <Header style={{ display: "flex", alignItems: "center" }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          items={new Array(5).fill(null).map((_, index) => {
-            const key = index + 1;
-            return {
-              key,
-              label: `nav ${key}`,
-            };
-          })}
-        />
+    <Layout className="layout ">
+      <Header className="bg-transparent sticky top-0 backdrop-blur-lg z-20 flex w-full items-center justify-between ">
+        <div className="flex items-center">
+          <Image src={logo} className="w-24 sm:w-32 rounded-md h-full" alt="logo" />
+          <Dropdown
+            className="ml-5"
+            menu={{
+              items,
+            }}
+            placement="bottomLeft"
+          >
+            <span className="cursor-pointer font-medium text-slate-600">
+              Category
+            </span>
+          </Dropdown>
+        </div>
+        <div>
+          <button
+            onClick={() => router.push("/pc-build")}
+            className="border-0 bg-blue-900 text-white py-1.5 sm:py-2 px-4 text-xs font-semibold rounded-full cursor-pointer hover:bg-blue-800 transition-all duration-200"
+          >
+            PC Builder
+          </button>
+        </div>
       </Header>
       <Content style={{ padding: "0 50px" }}>
-        <div
-          className="site-layout-content min-h-screen"
-        >
-          {children}
-        </div>
+        <div className="site-layout-content min-h-screen">{children}</div>
       </Content>
       <Footer style={{ textAlign: "center" }}>
         Ant Design ©2023 Created by Ant UED
---
+++ src/pages/pc-build/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PcBuildPage = () => {
+  return (
+    <div>
+      <h1>This is pc build page</h1>
+    </div>
+  );
+};
+
+export default PcBuildPage;
