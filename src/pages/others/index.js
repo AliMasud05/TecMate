c83d28a28353da94fb86a2047ba75353ca47bcbb
iamshakil19 <shakilahmed.pure@gmail.com>
2023-07-29 05:10:06 +0600

    others page done

--- src/pages/others/index.js
+++ src/pages/others/index.js
@@ -1,11 +1,33 @@
 import RootLayout from "@/components/Layout/RootLayout";
+import ProductCard from "@/components/ui/ProductCard";
 import React from "react";
 
-const OthersPage = () => {
-  return <div>this is others page</div>;
+const OthersPage = ({ others }) => {
+  return (
+    <div className="max-w-7xl mx-auto ">
+      <div className="mt-5">
+        <h2 className="text-center text-lg font-semibold mb-3">Others</h2>
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5 p-3">
+          {others?.map((product) => (
+            <ProductCard product={product} key={product._id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OthersPage;
 OthersPage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/products?category=others");
+  const data = await res.json();
+  return {
+    props: {
+      others: data,
+    },
+  };
 };
