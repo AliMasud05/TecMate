9cb8ca951ebb158cf509a830f6d14d03f0418ebc
iamshakil19 <shakilahmed.pure@gmail.com>
2023-07-29 04:34:39 +0600

    montherboard done

--- src/pages/motherboard/index.js
+++ src/pages/motherboard/index.js
@@ -1,11 +1,34 @@
 import RootLayout from "@/components/Layout/RootLayout";
+import ProductCard from "@/components/ui/ProductCard";
 
-
-const MotherboardPage = () => {
-  return <div>this is motherboard page</div>;
+const MotherboardPage = ({ motherboard }) => {
+  return (
+    <div className="max-w-7xl mx-auto ">
+      <div className="mt-5">
+        <h2 className="text-center text-lg font-semibold mb-3">Motherboard</h2>
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5 p-3">
+          {motherboard?.map((product) => (
+            <ProductCard product={product} key={product._id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MotherboardPage;
 MotherboardPage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "http://localhost:5000/products?category=motherboard"
+  );
+  const data = await res.json();
+  return {
+    props: {
+      motherboard: data,
+    },
+  };
 };
