2cac3b21d82b9a1761f938751efd184da06898f7
iamshakil19 <shakilahmed.pure@gmail.com>
2023-07-29 04:44:54 +0600

    ram page done

--- src/pages/ram/index.js
+++ src/pages/ram/index.js
@@ -1,10 +1,32 @@
 import RootLayout from "@/components/Layout/RootLayout";
+import ProductCard from "@/components/ui/ProductCard";
 
-const RamPage = () => {
-  return <div>this is ram page</div>;
+const RamPage = ({ rams }) => {
+  return (
+    <div className="max-w-7xl mx-auto ">
+      <div className="mt-5">
+        <h2 className="text-center text-lg font-semibold mb-3">Ram</h2>
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5 p-3">
+          {rams?.map((product) => (
+            <ProductCard product={product} key={product._id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RamPage;
 RamPage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/products?category=ram");
+  const data = await res.json();
+  return {
+    props: {
+      rams: data,
+    },
+  };
 };
