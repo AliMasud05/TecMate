46ab336b2422d0405719e4afc8c13bb2c1a29516
iamshakil19 <shakilahmed.pure@gmail.com>
2023-07-29 05:04:38 +0600

    power supply page done

--- src/pages/psu/index.js
+++ src/pages/psu/index.js
@@ -1,11 +1,33 @@
 import RootLayout from "@/components/Layout/RootLayout";
+import ProductCard from "@/components/ui/ProductCard";
 import React from "react";
 
-const PsuPage = () => {
-  return <div>this is psu image</div>;
+const PsuPage = ({ psu }) => {
+  return (
+    <div className="max-w-7xl mx-auto ">
+      <div className="mt-5">
+        <h2 className="text-center text-lg font-semibold mb-3">Power Supply</h2>
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5 p-3">
+          {psu?.map((product) => (
+            <ProductCard product={product} key={product._id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PsuPage;
 PsuPage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/products?category=psu");
+  const data = await res.json();
+  return {
+    props: {
+      psu: data,
+    },
+  };
 };
