b74ef8d92ed29a90eb227eab33f59d710e04acd3
iamshakil19 <shakilahmed.pure@gmail.com>
2023-07-29 12:44:58 +0600

    featured category page done

--- src/components/ui/ProductCard.js
+++ src/components/ui/ProductCard.js
@@ -1,7 +1,8 @@
+import { Rate } from "antd";
 import Image from "next/image";
 import { useRouter } from "next/router";
 const ProductCard = ({ product }) => {
-  const { _id, name, category, price, image, status } = product || {};
+  const { _id, name, category, price, image, status, rating } = product || {};
 
   let newCategory = null;
   if (category === "cpu") {
@@ -34,6 +35,21 @@ const ProductCard = ({ product }) => {
     newStatus = "Out Of Stock";
   }
   const router = useRouter();
+
+  const createSVGIcon = () => (
+    <svg
+      className="rating-icon"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 22 20"
+      fill="orange"
+      height="15px"
+      width="15px"
+    >
+      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+    </svg>
+  );
+
   return (
     <div
       onClick={() => router.push(`products/${_id}`)}
@@ -47,29 +63,32 @@ const ProductCard = ({ product }) => {
           alt=""
         />
       </div>
-        <div className="px-2">
-          <h1 className="font-semibold mt-1 mb-2">{name}</h1>
-        </div>
+      <div className="px-2">
+        <h1 className="font-semibold mt-1 mb-2">{name}</h1>
+      </div>
 
-        <div className="p-2">
-          <div className="flex justify-between">
-            <span className="">{newCategory}</span>
-            <span
-              className={`${
-                product.status === "inStock" ? "text-green-700" : "text-red-600"
-              } font-semibold`}
-            >
-              {newStatus}
-            </span>
-          </div>
-          <div className="flex justify-between mt-3">
-            <span className="">{product.rating}</span>
-            <span className="font-semibold text-red-500">
-              {product.price} <span className="">/-</span>
-            </span>
-          </div>
+      <div className="p-2">
+        <div className="flex justify-between">
+          <span className="">{newCategory}</span>
+          <span
+            className={`${
+              product.status === "inStock" ? "text-green-700" : "text-red-600"
+            } font-semibold`}
+          >
+            {newStatus}
+          </span>
         </div>
-
+        <div className="flex justify-between mt-3">
+          <span className="flex">
+            {Array.from({ length: rating }).map((_, index) => (
+              <span className="" key={index}>{createSVGIcon()}</span>
+            ))}
+          </span>
+          <span className="font-semibold text-red-500">
+            {product.price} <span className="">/-</span>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
