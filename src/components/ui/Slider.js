5e51acea28cadf39529393317e9e3d4957f4682b
AliMasud05 <morshedalimasud3@gmail.com>
2023-08-01 01:15:10 +0600

    server

--- src/components/ui/Slider.js
+++ src/components/ui/Slider.js
@@ -6,45 +6,31 @@ import sliderImg3 from "../../assets/3.webp";
 import Image from "next/image";
 const HomeSlider = () => {
   return (
-    // <div className="sm:mt-5 w-full">
-    //   <Carousel autoplay effect="fade">
-    //     <div className="">
-    //       <Image
-    //         src={sliderImg1}
-    //         alt=""
-    //         className="mx-auto h-[600px] container max-w-5xl"
-    //       />
-    //     </div>
-    //     <div>
-    //       <Image
-    //         src={sliderImg2}
-    //         alt=""
-    //         className="mx-auto container max-w-5xl h-[600px]"
-    //       />
-    //     </div>
-    //     <div>
-
-    //     </div>
-    //   </Carousel>
-    // </div>
-
-    <Carousel>
-      <Image
-        src={sliderImg3}
-        alt=""
-        className="mx-auto container max-w-5xl h-[600px]"
-      />
-      <Image
-        src={sliderImg2}
-        alt=""
-        className="mx-auto container max-w-5xl h-[600px]"
-      />
-      <Image
-        src={sliderImg3}
-        alt=""
-        className="mx-auto container max-w-5xl h-[600px]"
-      />
-    </Carousel>
+    <div className="sm:mt-5 w-full">
+      <Carousel autoplay effect="fade">
+        <div className="">
+          <Image
+            src={sliderImg1}
+            alt=""
+            className="mx-auto h-[600px] container max-w-5xl"
+          />
+        </div>
+        <div>
+          <Image
+            src={sliderImg2}
+            alt=""
+            className="mx-auto container max-w-5xl h-[600px]"
+          />
+        </div>
+        <div>
+          <Image
+            src={sliderImg3}
+            alt=""
+            className="mx-auto container max-w-5xl h-[600px]"
+          />
+        </div>
+      </Carousel>
+    </div>
   );
 };
 
